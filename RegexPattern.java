59b60c3db691ece99f421c0338127843b71f67c0
sharale34 <sourabhharale8000@gmail.com>
2020-09-30 10:09:03 +0530

    Added: Welcome Message is Added

---
+++ RegexPattern.java
@@ -0,0 +1,10 @@
+package com.BridgeLabz.regex;
+
+public class RegexPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Welcome To PINCODE Validation Program");
+	}
+
+}
